caffab2f0127a2c9435e15542608efa5ccd535ac
Maurizio Branca <mbranca@tierratelematics.com>
2017-06-20 03:00:12 +0200

    Add EC2 instances FQDNs and private IPs to outputs

--- modules/ecs-cluster/outputs.tf
+++ modules/ecs-cluster/outputs.tf
@@ -5,3 +5,11 @@
 output "ecs_cluster_id" {
   value = "${aws_ecs_cluster.ecs_cluster.id}"
 }
+
+output "ecs_cluster_instance_fqdns" {
+  value = "${aws_route53_record.internal_dns_record.*.fqdn}"
+}
+
+output "ecs_cluster_instance_private_ips" {
+  value = "${aws_instance.ecs_instance.*.private_ip}"
+}
